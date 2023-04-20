dfab36d6cd9129d301974253c15afa6f66896ac1
Valentina Perishko <valentina.perishko@gmail.com>
2023-04-19 20:43:38 -0600

    adds changes

---
+++ src/js/02-timer.js
@@ -0,0 +1,69 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const startBtn = document.querySelector('button[data-start]');
+const day = document.querySelector('[data-days]');
+const hour = document.querySelector('[data-hours]');
+const minute = document.querySelector('[data-minutes]');
+const second = document.querySelector('[data-seconds]');
+
+let timerId = null;
+let selectedDate = null;
+let countdown = 0;
+startBtn.disabled = true;
+
+flatpickr('#datetime-picker', {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    selectedDate = selectedDates[0];
+    if (selectedDates[0] < new Date()) {
+      Notify.failure('Please choose a date in the future!');
+    } else {
+      startBtn.disabled = false;
+    }
+  },
+});
+
+startBtn.addEventListener('click', onStartBtnClick);
+
+function onStartBtnClick() {
+  timerId = setInterval(() => {
+    countdown = selectedDate - new Date();
+    updateTimer(convertMs(countdown));
+    if (countdown <= 1000) {
+      clearInterval(timerId);
+    }
+  }, 1000);
+}
+
+function updateTimer({ days, hours, minutes, seconds }) {
+  day.textContent = addLeadingZero(days);
+  hour.textContent = addLeadingZero(hours);
+  minute.textContent = addLeadingZero(minutes);
+  second.textContent = addLeadingZero(seconds);
+}
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+
+  const hours = Math.floor((ms % day) / hour);
+
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return value.toString().padStart(2, '0');
+}
