ba93f77aa2c5a5ae2d8736668f3f844eadc3665d
valentinaperishko <69521682+valentinaperishko@users.noreply.github.com>
2023-04-20 02:44:14 +0000

    Deploying to gh-pages from @ valentinaperishko/goit-js-hw-09@dfab36d6cd9129d301974253c15afa6f66896ac1 🚀

---
+++ 01-color-switcher.e21c7af7.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body"),a=null;t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,a=setInterval((function(){n.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3)})),e.addEventListener("click",(function(){e.disabled=!0,t.disabled=!1,a=clearInterval(a)}))}();
+//# sourceMappingURL=01-color-switcher.e21c7af7.js.map
